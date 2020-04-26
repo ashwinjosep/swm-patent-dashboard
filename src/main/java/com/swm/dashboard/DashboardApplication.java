2d8fd93fb49cb29b5cde44904befb0e1a82f85d4
ashwinjosep <ashwinjosep94@gmail.com>
2020-04-26 13:44:29 -0700

    initial commit

---
+++ src/main/java/com/swm/dashboard/DashboardApplication.java
@@ -0,0 +1,14 @@
+package com.swm.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
